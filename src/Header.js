f0352673ca3cbcd53fcee288de51b44b4f4d1622
Sam Peck <fullstacksam@protonmail.com>
2023-06-05 21:18:05 -0400

    starting the header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,22 @@
-function Header(){
+function Header({setSignedIn, signedIn}){
+
+        function handleLiClick(){
+
+            setSignedIn((signedIn)=> !signedIn)
+
+                                    }
 
     return (
-
-        <Header />
-
-
+        <div>
+            <ul className="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">User Universe</a></li>
+                <li><a href="#">idk yet</a></li>
+                <li onClick={handleLiClick} ><a href="#">{signedIn ?" Sign out" : "Sign In"}</a></li>
+           </ul>
+        </div>
     )
 
 }
+
+ export default Header
