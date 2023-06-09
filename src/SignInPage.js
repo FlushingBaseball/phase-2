6bf440b5612c60a3c6fa267c8dd73ce84d2a3b07
Jamal Portericker <jportericker@yahoo.com>
2023-06-09 09:55:07 -0400

    update

--- src/SignInPage.js
+++ src/SignInPage.js
@@ -1,78 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import {BrowserRouter as Router, useHistory } from 'react-router-dom';
-// import NewUser from './NewUser';
-
-// function SignInPage() {
-//   const [authorized, setAuthorized] = useState(false);
-//   const [users, setUsers] = useState([]);
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   useEffect(() => {
-//     fetchUsers();
-//   }, []);
-
-//   async function fetchUsers() {
-//     try {
-//       const response = await fetch('http://localhost:3001/Users');
-//       const data = await response.json();
-//       setUsers(data);
-//     } catch (error) {
-//       console.error('Error fetching user data:', error);
-//     }
-//   }
-
-//   const history = useHistory();
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     const auth = users.some((user) => user.username === username && user.password === password);
-//     setAuthorized(auth);
-//     if (auth) {
-//       history.push('/');
-//     } else {
-//       alert('Sorry Wrong username or password')
-//     }
-//   }
-
-//   const login = (
-//     <form action="#" onSubmit={handleSubmit}>
-//       <input
-//         type="text"
-//         placeholder="Username"
-//         value={username}
-//         onChange={(e) => setUsername(e.target.value)}
-//         autoComplete="username"
-//       />
-//       <input
-//         type="password"
-//         placeholder="Password"
-//         value={password}
-//         onChange={(e) => setPassword(e.target.value)}
-//       />
-//       <input type="submit" value="Log In" />
-//       <p>Don't have an account?</p>
-//       <NewUser />
-//     </form>
-//   );
-
-//   return (
-//     <div id="authorization" className="container">
-//         {authorized ? (
-//           <div>
-//           </div>
-//         ) : (
-//           <div>
-//             <h2>Login</h2>
-//             {login}
-//           </div>
-//         )}
-//     </div>
-//   );
-// }
-
-// export default SignInPage;
-
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, useHistory, Link } from 'react-router-dom';
 
@@ -142,7 +67,7 @@ function SignInPage() {
         <p style={{ color: 'red' }}>Sorry, wrong username or password</p>
       )}
       <p>
-        Don't have an account? <Link to="/sign-Up">Sign up</Link>
+        Don't have an account? <Link to="/sign-Up" >Sign up</Link>
       </p>
     </form>
   );
@@ -162,5 +87,4 @@ function SignInPage() {
     </div>
   );
 }
-
 export default SignInPage;
